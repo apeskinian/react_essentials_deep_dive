96d242051f3df02725cf2b42d0b8ecd604310911
Matthew Aitken <matt@itken.net>
2025-06-05 14:35:08 +0100

    Make names editable

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,31 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [ playerName, setPlayerName ] = useState(initialName);
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEdit() {
+        setIsEditing(editing => !editing)
+    }
+
+    // Takes a new name from the input field and sets the name with useState.
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    // This sets to an input field and listens to changes which are sent to useState.
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+    }
+
     return (
     <li>
         <span className="player">
-            <span className="player-name">{name}</span>
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
     );
 }
