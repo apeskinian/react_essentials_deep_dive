9a60e21cdf2f69f0d21def03d5de86233a33d25c
Matthew Aitken <matt@itken.net>
2025-06-05 14:50:06 +0100

    Add comments to code

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function Player({ initialName, symbol }) {
+    // playerName takes the default value from the initialName prop
     const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
 
