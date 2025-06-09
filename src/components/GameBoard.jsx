9cf946dd935b1e6d0fce4d05b2b9285d7c08d603
Matthew Aitken <matt@itken.net>
2025-06-09 15:46:36 +0100

    Add dynamic button disabling

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -40,7 +40,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
         {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => <li key={{colIndex}}>
-                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                        {/* disabled prop is dynamic based on if player symbol is equal to null */}
+                        {playerSymbol}
+                    </button>
                 </li>)}
             </ol>
         </li>)}
