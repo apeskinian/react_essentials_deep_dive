cbae4c21e8d0b3eba7927b6d2dc6e62941323478
Matthew Aitken <matt@itken.net>
2025-06-09 13:36:29 +0100

    Add function to update gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const InitialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,11 +7,25 @@ const InitialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(InitialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            // creates a copy of the prevGameBoard array and use .map to make copies of the inner arrays too.
+            // This updates the array in an immutable way which is best practise.
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            // Replace selected square with symbol..
+            updatedGameBoard[rowIndex][colIndex] = 'X';
+            // Return new board
+            return updatedGameBoard;
+        });
+    }
+
     return <ol id="game-board">
-        {InitialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => <li key={{colIndex}}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
